ac4f9305564ec3afe04870b25135ccbea4a0c268
mike-mirabal <mikemirabal360@gmail.com>
2025-07-02 22:06:51 -0500

    slider edits

--- scripts/project.js
+++ scripts/project.js
@@ -213,17 +213,11 @@ function renderDetail(p, container) {
   }
 
   const swiper = new Swiper('.swiper', {
-    effect: 'coverflow',
+    effect: 'fade',
     grabCursor: true,
     centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
+    slidesPerView: 1,
+    loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
